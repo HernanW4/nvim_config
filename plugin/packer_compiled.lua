e74ede05a05ac3f207d1528c91becf22e5ba4bd8
HernanW4 <zhernandezwalter@gmail.com>
2022-09-19 18:56:17 -0400

    added barbar as the tab plugin

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/home/walter-arch/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  ["barbar.nvim"] = {
+    loaded = true,
+    path = "/home/walter-arch/.local/share/nvim/site/pack/packer/start/barbar.nvim",
+    url = "https://github.com/romgrk/barbar.nvim"
+  },
   ["cmp-buffer"] = {
     loaded = true,
     path = "/home/walter-arch/.local/share/nvim/site/pack/packer/start/cmp-buffer",
